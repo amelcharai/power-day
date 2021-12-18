52c5ff2ffa5810249904aa89187a67fad6455015
Amel <amel.toronto@gmail.com>
2021-12-17 22:43:34 -0500

    feat: open ref link to new tab

--- src/containers/App.js
+++ src/containers/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a className="App-reference" href='https://pngtree.com/so/Hand'>Hand png from pngtree.com/</a>
+          <a className="App-reference" href='https://pngtree.com/so/Hand' target="_blank">Hand png from pngtree.com/</a>
           <h1>Power Day</h1>
         </header>
         <Scroll>
